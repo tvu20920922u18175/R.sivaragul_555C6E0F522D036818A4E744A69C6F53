2599302b15b765f8a58a0c4e5f532744da88fde8
tvu20920922u18175 <147379096+tvu20920922u18175@users.noreply.github.com>
2023-10-09 13:25:33 +0530

    Add files via upload

---
+++ Challenge(1).py
@@ -0,0 +1,13 @@
+#Implement a recursive function to calculate the factorial of a given number.
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+# Example usage:
+num = 5
+result = factorial(num)
+print(f"The factorial of {num} is {result}")
---
+++ Challenge(2).py
@@ -0,0 +1,10 @@
+#Write a program that determines whether a year entered by the user is a leap year or not using if- elif-else statements.
+
+# Input year from the user
+year = int(input("Enter a year: "))
+
+# Check if it's a leap year
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")
